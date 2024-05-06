d73b36e48a6e00a878ac75a238e5c02ee39ec153
OliGarc333 <ngarcia333@gmail.com>
2024-05-06 08:27:47 +0200

    DB Schemas completed

--- src/collections/ProductFile.ts
+++ src/collections/ProductFile.ts
@@ -7,33 +7,54 @@ const addUser: BeforeChangeHook = ({ req, data }) => {
   return { ...data, user: user?.id };
 };
 
-// const yourOwnAndPurchase: Access = async ({ req }) => {
-//   const user = req.user as User;
+const yourOwnAndPurchase: Access = async ({ req }) => {
+  const user = req.user as User;
 
-//   if (user?.role === 'admin') return true;
-//   if (!user) return false;
+  if (user?.role === 'admin') return true;
+  if (!user) return false;
 
-//   const { docs: products } = await req.payload.find({
-//     collection: 'products',
-//     depth: 0,
-//     where: {
-//       user: {
-//         equals: user.id
-//       }
-//     }
-//   });
-//   const ownProductFileIds = products.map(prod => prod.product_files).flat();
+  const { docs: products } = await req.payload.find({
+    collection: 'products',
+    depth: 0,
+    where: {
+      user: {
+        equals: user.id
+      }
+    }
+  });
+  const ownProductFileIds = products.map(prod => prod.product_files).flat();
 
-//   const { docs: orders } = await req.payload.find({
-//     collection: 'orders',
-//     depth: 0,
-//     where: {
-//       user: {
-//         equals: user.id
-//       }
-//     }
-//   });
-// };
+  const { docs: orders } = await req.payload.find({
+    collection: 'orders',
+    depth: 2,
+    where: {
+      user: {
+        equals: user.id
+      }
+    }
+  });
+
+  const purchasedProductFileIds = orders
+    .map(order => {
+      return order.products.map(product => {
+        if (typeof product === 'string')
+          return req.payload.logger.error(
+            'Search depth not sufficient to find purchase filed IDs'
+          );
+        return typeof product.product_files === 'string'
+          ? product.product_files
+          : product.product_files.id;
+      });
+    })
+    .filter(Boolean)
+    .flat();
+
+  return {
+    id: {
+      in: [...ownProductFileIds, ...purchasedProductFileIds]
+    }
+  };
+};
 
 export const ProductFile: CollectionConfig = {
   slug: 'product_files',
@@ -43,9 +64,11 @@ export const ProductFile: CollectionConfig = {
   hooks: {
     beforeChange: [addUser]
   },
-  //   access: {
-  //     read: yourOwnAndPurchase()
-  //   },
+  access: {
+    read: yourOwnAndPurchase,
+    update: ({ req }) => req.user.role === 'admin',
+    delete: ({ req }) => req.user.role === 'admin'
+  },
   upload: {
     staticURL: '/product_files',
     staticDir: 'product_files',
