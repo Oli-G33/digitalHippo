433c02415bc68c564d626be7578a76f8e0c08455
OliGarc333 <ngarcia333@gmail.com>
2024-05-06 21:50:59 +0200

    Implementing ProductReel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import MaxWidthWrapper from '@/components/MaxWidthWrapper';
+import ProductReel from '@/components/ProductReel';
 import { Button, buttonVariants } from '@/components/ui/Button';
 import { ArrowDownToLine, CheckCircle, Leaf } from 'lucide-react';
 import Link from 'next/link';
@@ -44,7 +45,12 @@ export default function Home() {
             <Button variant="ghost">Our quality promise &rarr;</Button>
           </div>
         </div>
-        {/**TODO: List products */}
+        <ProductReel
+          title="Brand New"
+          subtitle=""
+          href="/products"
+          query={{ sort: 'desc', limit: 4 }}
+        />
       </MaxWidthWrapper>
       <section className="border-t border-gray-200 bg-gray-50">
         <MaxWidthWrapper className="py-20">
