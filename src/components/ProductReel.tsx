91142cb21df37c185b601a84d16c469c09a82138
OliGarc333 <ngarcia333@gmail.com>
2024-05-07 22:07:36 +0200

    Implementing ProductListing

--- src/components/ProductReel.tsx
+++ src/components/ProductReel.tsx
@@ -1,9 +1,10 @@
 'use client';
 
-import { TQueryValidator } from '@/lib/validators/Query-Validator';
 import { Product } from '@/payload-types';
 import { trpc } from '@/trpc/client';
 import Link from 'next/link';
+import ProductListing from './ProductListing';
+import { TQueryValidator } from '@/lib/validators/Query-Validator';
 
 interface ProductReelProps {
   title: string;
@@ -13,24 +14,28 @@ interface ProductReelProps {
 }
 
 const FALLBACK_LIMIT = 4;
+
 const ProductReel = (props: ProductReelProps) => {
   const { title, subtitle, href, query } = props;
 
-  const { data: queryResults } = trpc.getInfiniteProducts.useInfiniteQuery(
-    {
-      limit: query.limit ?? FALLBACK_LIMIT,
-      query
-    },
-    {
-      getNextPageParam: lastPage => lastPage.nextPage
-    }
-  );
+  const { data: queryResults, isLoading } =
+    trpc.getInfiniteProducts.useInfiniteQuery(
+      {
+        limit: query.limit ?? FALLBACK_LIMIT,
+        query
+      },
+      {
+        getNextPageParam: lastPage => lastPage.nextPage
+      }
+    );
 
   const products = queryResults?.pages.flatMap(page => page.items);
 
   let map: (Product | null)[] = [];
   if (products && products.length) {
     map = products;
+  } else if (isLoading) {
+    map = new Array<null>(query.limit ?? FALLBACK_LIMIT).fill(null);
   }
 
   return (
@@ -46,6 +51,7 @@ const ProductReel = (props: ProductReelProps) => {
             <p className="mt-2 text-sm text-muted-foreground">{subtitle}</p>
           ) : null}
         </div>
+
         {href ? (
           <Link
             href={href}
@@ -55,10 +61,17 @@ const ProductReel = (props: ProductReelProps) => {
           </Link>
         ) : null}
       </div>
+
       <div className="relative">
         <div className="mt-6 flex items-center w-full">
           <div className="w-full grid grid-cols-2 gap-x-4 gap-y-10 sm:gap-x-6 md:grid-cols-4 md:gap-y-10 lg:gap-x-8">
-            {}
+            {map.map((product, i) => (
+              <ProductListing
+                key={`product-${i}`}
+                product={product}
+                index={i}
+              />
+            ))}
           </div>
         </div>
       </div>
