86bcac5b438f9456f29bd59beff7340f8ed47437
Oli-G33 <ngarcia333@gmail.com>
2024-05-30 11:32:38 +0200

    more changes for deployment on vercel

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -1,13 +1,18 @@
 import { appRouter } from '@/trpc';
 import { fetchRequestHandler } from '@trpc/server/adapters/fetch';
 
-const handler = (req: Request) => {
-  fetchRequestHandler({
+const handler = async (req: Request) => {
+  const response = await fetchRequestHandler({
     endpoint: '/api/trpc',
     req,
     router: appRouter,
     // @ts-expect-error context is passed from Express middleware
     createContext: () => ({})
+  });
+
+  return new Response(response.body, {
+    status: response.status,
+    headers: response.headers
   });
 };
 
